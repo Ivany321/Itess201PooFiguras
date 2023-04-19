caf7218945d8bb452c334e0c0271dff1ced309c5
ivanypower@gmail.com <>
2023-04-19 16:05:06 -0500

    Proyecto de dibujo de figuras usando clase abstracta y herencia. Clase cuadrado 1 funcional Clase triangulo 1 funcional

---
+++ src/figuras/Figura.java
@@ -0,0 +1,12 @@
+package figuras;
+
+
+public abstract class Figura {
+    protected int n;
+
+    public void setN(int n) {
+        this.n = n;
+    }
+    
+    public abstract void imprimir();
+}
---
+++ test/figuraTest.java
@@ -0,0 +1,16 @@
+import figuras.Cuadrado1;
+import figuras.Triangulo1;
+public class figuraTest {
+    public static void main(String[]args){
+        Cuadrado1 c1 = new Cuadrado1();
+        Triangulo1 t1 = new Triangulo1();
+        
+        c1.setN(6);
+        c1.imprimir();
+        
+        t1.setN(6);
+        t1.imprimir();
+        
+    }
+    
+}
